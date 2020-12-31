c5a290df6191bc18998b0b6a4df9a9cb0c75f0b8
Crowdbotics <team@crowdbotics.com>
2020-12-31 08:48:52 +0000

    remove package blueprint-react-native-addItem-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,7 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
 { name: 'Maps189346', human_name: 'Maps', access_route: 'Maps189346', icon: 'map'},
-{ name: 'Additem189345', human_name: 'AddItem', access_route: 'Additem189345'},
 { name: 'Maps189341', human_name: 'Maps', access_route: 'Maps189341', icon: 'map'},
 { name: 'UserProfile189337', human_name: 'User Profile', access_route: 'UserProfile189337'},
 
